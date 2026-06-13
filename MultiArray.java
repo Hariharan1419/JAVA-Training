4c2b7cf45c10cc5b7c78504e54fdf0e10921a548
Hariharan1419 <mailmehari1111@gmail.com>
2026-06-13 14:22:14 +0530

    Implement MultiArray to calculate product of integers in the given array using i--
    This program calculates the product of an array of integers and prints the result of dividing the product by each element, skipping zeros.

---
+++ MultiArray.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class MultiArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " numbers: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+       
+        int product = 1;
+        for (int i = 0; i < n; i++) {
+            product *= arr[i];
+        }
+
+        System.out.println("Output:");
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                System.out.print((product / arr[i]) + " ");
+            } 
+        }
+    }
+}
